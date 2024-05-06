283a6e68f60fd1b6b114eb2e4512202b78fea614
lindotex <lindoten@gmail.com>
2024-05-06 01:40:02 +0100

    atualizacao no sistema

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from add import Adicionar
 from remover import Remover
 from read import Exibir
 from atualizar import Atualizar
+from prettytable import PrettyTable
 
 def limpar_terminal():
     print('\033c', end='')
@@ -11,29 +12,36 @@ def limpar_terminal():
 loop_on = True
 
 while loop_on == True:
+    t = PrettyTable(['Numero','Opcao'])
     print('SISTEMA DE CONTROLE')
-    print('')
     print('Projeto de CRUD com Python e MongoDB')
     print('')
     print('Escolha entre as opcoes:')
-    print('1 - Criar')
-    print('2 - Deletar')
-    print('3 - Exibir ')
-    print('4 - Atualizar ')
-    print('5 - Sair ')
-    escolha = input("Digite uma opção (1, 2 ou 3): ")
+    t.add_row(['1','Criar'])
+    t.add_row(['2','Deletar'])
+    t.add_row(['3','Exibir'])
+    t.add_row(['4','Atualizar'])
+    t.add_row(['5','Sair'])
+    print(t)
+    escolha = input("Digite uma opção (1, 2, 3, 4 ou 5): ")
         
     if escolha == '1':
+        print(f'Opcao selecionada: {escolha}... aguarde.')
+        time.sleep(2)
         limpar_terminal()
         adicionar = Adicionar
         adicionar.adicionar()
         
     if escolha == '2':
+        print(f'Opcao selecionada: {escolha}... aguarde.')
+        time.sleep(2)
         limpar_terminal()
         remover = Remover
         remover.remover()
         
     if escolha == '3':
+        print(f'Opcao selecionada: {escolha}... aguarde.')
+        time.sleep(2)
         set_freeze = True
         limpar_terminal()
         while set_freeze:
@@ -43,14 +51,23 @@ while loop_on == True:
             get_menu = input()
             if (get_menu == 'Y') or (get_menu== 'y'):
                 set_freeze = False
+            if (get_menu == 'N') or (get_menu== 'n'):
+                print('Estamos encerrando o sistema.')
+                time.sleep(2)
+                set_freeze = False
+                loop_on = False  
         limpar_terminal()
         
     if escolha == '4':
+        print(f'Opcao selecionada: {escolha}... aguarde.')
+        time.sleep(2)
         limpar_terminal()
         atualizar = Atualizar
         atualizar.atualizar()
         
     if escolha == '5':
+        print(f'Opcao selecionada: {escolha}... aguarde.')
+        time.sleep(2)
         limpar_terminal()
         loop_on = False  
     
@@ -63,6 +80,9 @@ while loop_on == True:
         time.sleep(1)
         print('.....')
         time.sleep(1)
+        print(f'Valor {escolha} nao e uma opcao valida, estamos retornando ao menu principal...')
+        time.sleep(2)
+        limpar_terminal()
 
     
 print("Voce Finalizou o sistema.")
--- read.py
+++ read.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv, find_dotenv
 from conn import ConexaoMongoDB
 import os
+from prettytable import PrettyTable
 
 class Exibir:
     def __init__(self) -> None:
@@ -16,12 +17,15 @@ class Exibir:
         conexao.quantidade_na_collection('test')
         print("")
         resultados = conexao.executar_query("test", {})
+        t = PrettyTable(['ID','Nome','Ocupacao'])
         if resultados:
             for documento in resultados:
+                _id = documento.get("_id", "_Id não encontrado")
                 nome = documento.get("name", "Nome não encontrado")
                 ocupacao = documento.get("ocupation", "Ocupação não encontrada")
-                print(f"Nome: {nome}, Ocupação: {ocupacao}")
-            print("")    
+                t.add_row([_id,nome,ocupacao])
+                # print(f"ID: {_id} | Nome:  {nome} | Ocupação: {ocupacao}")
+            print(t)    
         else:
             print("Nenhum documento encontrado na coleção 'test'.")
         conexao.desconectar()   
